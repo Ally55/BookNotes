9f73df7c92eb00627e5a85b45438cff979f3abbd
andreea.dobrin781@gmail.com <andreea.dobrin781@gmail.com>
2020-10-30 16:52:42 +0200

    Add Composer autoloader

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 
+require_once __DIR__ . '/../vendor/autoload.php';
 require_once '../src/databases/database.php';
 require_once '../src/auth.php';
 require  '../src/validator.php';
