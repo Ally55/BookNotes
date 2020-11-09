d8b7cc770570caeb6bfac0df9d726bec96b699ab
andreea.dobrin781@gmail.com <andreea.dobrin781@gmail.com>
2020-11-09 20:21:32 +0200

    Remove unnecessary new lines

--- app/views/404.php
+++ app/views/404.php
@@ -1,14 +1,8 @@
 
 <div class="container-fluid error-container d-flex justify-content-center align-items-center flex-column p-0">
-
-<!--        <div class="col bg-warning error404">-->
-<!--            <h1> 404</h1>-->
-<!--        </div>-->
         <h1 class="error-404-message text-dark">Ooops... Something went wrong here..</h1>
         <div class="row no-gutters d-flex align-items-center mt-5 error-child-container">
-                <h5 class="error-404-advice text-dark">We can't find the page you are looking for. Please go back to home.</h5>
-    <!--        <button type="button" class="btn btn-outline-dark error-button ml-5"><a href="/" class="text-decoration-none text-dark a-button">Home</a></button>-->
+            <h5 class="error-404-advice text-dark">We can't find the page you are looking for. Please go back to home.</h5>
             <a href="/" type="button" class="btn btn-outline-dark error-button p-2">Home</a>
         </div>
-
 </div>
